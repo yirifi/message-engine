3c2de73274b690d1c4604ec212d61ad7fc17ddff
Sajesh Adhikari <Sajesh.Adhikari@cedargate.com>
2024-03-25 22:36:22 +0545

    minor refactorings
    1. removed dist/ from repo, included it in the gitignore file
    2. updated scripts in package.json file

--- dist/config/zeptoMailConfig.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TEMPLATE_API_URL = exports.API_URL = void 0;
-exports.API_URL = "api.zeptomail.eu/";
-exports.TEMPLATE_API_URL = "api.zeptomail.eu/v1.1/email/template/";
-//# sourceMappingURL=zeptoMailConfig.js.map
